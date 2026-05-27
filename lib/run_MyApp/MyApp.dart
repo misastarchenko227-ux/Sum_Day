f43a34be34892ef0db94107f3ad0372ac675e1d4
Михаил <misastarchenko227@gmail.com>
2026-05-27 15:57:15 +0500

    подключение сервара с приложением

--- lib/run_MyApp/MyApp.dart
+++ lib/run_MyApp/MyApp.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sum_day/registration/registration_mainScren.dart';
-void main() {
+import 'package:supabase_flutter/supabase_flutter.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://jcnycdmjawnvlmrdlsxn.supabase.co',
+    anonKey: 'sb_publishable_giSuoz_HYC_-j0ggxSoHzA_pkezdZGz',
+  );
   runApp(const MyApp());
 }
 
